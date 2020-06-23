3f7f98060a669171509d50b1f173fbf70616e040
Sky McMahon <sky.m.mcmahon@gmail.com>
2020-06-23 17:43:09 -0600

    Timer added and tied to some functions

---
+++ js/app.js
@@ -0,0 +1,75 @@
+class tomagotchi{
+    constructor(hungerResistance, tiredResistance, bordomResistance){
+        this.hungerResistance = hungerResistance || Math.floor((Math.random() * (.2 - .15 + .1) + .15)* 10) /10;
+        this.tiredResistance = tiredResistance || Math.floor((Math.random() * (.2 - .15 + .1) + .15)* 10) /10;
+        this.bordomResistance = bordomResistance ||  Math.floor((Math.random() * (.2 - .15 + .1) + .15)* 10) /10;
+        this.hunger = 0;
+        this.tiredness = 0;
+        this.bordom = 0;
+        this.age = 0;
+        this.name = '';
+    }
+    //----------methods----------
+       //eat
+    eat() {
+        this.hunger = 0;
+    }
+       //sleep
+    sleep() {
+        this.tiredness = 0;
+    }
+       //play
+    play() {
+        this.bordom = 0;
+    }
+};
+//----------Universal stuff----------
+let time = 0;
+
+
+const billy = new tomagotchi();
+console.log(billy);
+
+
+//----------functions----------
+function updateAge() {
+    const age = document.getElementById('age');
+    age.innerText = `Billy: ${age}s`;
+};
+
+function updateStats() {
+    billy.bordom += 1;
+    billy.hunger += 1;
+    billy.tiredness += 1;
+};
+
+
+function startTimer(){
+    const timer = setInterval(function (){
+        if (time < 60){
+            time++;
+            console.log(time);
+            if (time % 3 === 0){
+                updateStats();
+            console.log('stats updated');
+            }else if(time % 10 === 0){
+                updateAge();
+                console.log('age updated');
+            }
+        // // }else if (time === 10){
+        // //     updateAge();
+        // }else if (time % 3 === 0){
+        //     updateStats();
+        //     console.log('stats updated');
+        } 
+        else{
+            console.log('Time is up');
+            console.log(billy);
+            clearInterval(timer);
+        }
+    }, 1000);
+}
+
+
+//----------temp----------
+startTimer()
